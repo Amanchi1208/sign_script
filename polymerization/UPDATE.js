4e1fa54dbd28651d2fb73157a46e05a3c486b1a6
imoki <78804251+imoki@users.noreply.github.com>
2024-02-08 19:04:26 +0800

    update

--- polymerization/UPDATE.js
+++ polymerization/UPDATE.js
@@ -1,5 +1,5 @@
 // UPDATE.js 更新脚本
-// 20231115
+// 20240208
 
 // 当前分配置表：
 // 可用脚本：
@@ -7,6 +7,7 @@
 // wps(稻壳版）、网易云游戏、什么值得买、在线工具
 // 叮咚买菜-叮咚果园、叮咚买菜-叮咚鱼塘
 // 北京时间、AcFun、喜马拉雅
+// ios游戏迷
 
 // 失效脚本：
 // 吾爱破解、 wps(轻量版)、wps(客户端版)
@@ -22,7 +23,7 @@ var emailWorkbook = 'EMAIL' // 邮箱表的名称
 // 分配置表名称
 var subConfigWorkbook=['aliyundrive_multiuser','52pojie','noteyoudao','wps','tieba',
 'wangyiyungame', 'smzdm', 'toollu', 'cake', 'tianrun', 
-'xifushe', 'ddmc', 'everphoto', 'btime','acfun','xmly'];
+'xifushe', 'ddmc', 'everphoto', 'btime','acfun','xmly', 'tonghua'];
 var workbook = [] // 存储已存在表数组
 
 // 表中激活的区域的行数和列数
@@ -54,6 +55,7 @@ var configContent=[
   ['btime','北京时间','否','否'],
   ['acfun','AcFun','否','否'],
   ['xmly','喜马拉雅','否','否'],
+  ['tonghua','ios游戏迷','否','否'],
 ]
 
 // PUSH表内容 		
