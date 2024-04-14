88d615783da8e1c3e2c5542530144c647d09cf4d
imoki <78804251+imoki@users.noreply.github.com>
2024-04-14 14:42:20 +0800

    Update UPDATE.js

--- polymerization/UPDATE.js
+++ polymerization/UPDATE.js
@@ -1,5 +1,5 @@
 // UPDATE.js 更新脚本
-// 20240412
+// 20240414
 
 // 当前分配置表：
 // 可用脚本：
@@ -7,7 +7,7 @@
 // wps(稻壳版）、网易云游戏、什么值得买、在线工具
 // 叮咚买菜-叮咚鱼塘
 // 北京时间、AcFun、喜马拉雅
-// ios游戏迷、希沃白板
+// ios游戏迷、希沃白板、小木虫
 
 // 失效脚本：
 // 吾爱破解、 wps(轻量版)、wps(客户端版)
@@ -24,7 +24,7 @@ var emailWorkbook = 'EMAIL' // 邮箱表的名称
 // 分配置表名称
 var subConfigWorkbook=['aliyundrive_multiuser','52pojie','noteyoudao','wps','tieba',
 'wangyiyungame', 'smzdm', 'toollu', 'cake', 'tianrun', 
-'xifushe', 'ddmc', 'everphoto', 'btime','acfun','xmly', 'tonghua', 'en'];
+'xifushe', 'ddmc', 'everphoto', 'btime','acfun','xmly', 'tonghua', 'en', 'xmc'];
 var workbook = [] // 存储已存在表数组
 
 // 表中激活的区域的行数和列数
@@ -57,6 +57,8 @@ var configContent=[
   ['acfun','AcFun','否','否'],
   ['xmly','喜马拉雅','否','否'],
   ['tonghua','ios游戏迷','否','否'],
+  ['en','希沃白板','否','否'],
+  ['xmc','小木虫','否','否'],
 ]
 
 // PUSH表内容 		
