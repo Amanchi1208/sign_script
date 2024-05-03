052bd775d2b0f7ce0003d12e8d35b1635d2c940d
imoki <78804251+imoki@users.noreply.github.com>
2024-05-03 12:41:59 +0800

    Update easinote.js

--- polymerization/easinote.js
+++ polymerization/easinote.js
@@ -1,5 +1,5 @@
 // 希沃白板自动签到
-// 20240412
+// 20240503
 
 let sheetNameSubConfig = "en"; // 分配置表名称
 let pushHeader = "【希沃白板】";
@@ -149,7 +149,8 @@ function bark(message, key) {
 // 推送pushplus消息
 function pushplus(message, key) {
   if (key != "") {
-    url = "http://www.pushplus.plus/send?token=" + key + "&content=" + message;
+    // url = "http://www.pushplus.plus/send?token=" + key + "&content=" + message;
+    url = "http://www.pushplus.plus/send?token=" + key + "&content=" + message + "&title=" + pushHeader;  // 增加标题
     let resp = HTTP.fetch(url, {
       method: "get",
     });
